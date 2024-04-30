aaed64b75e8cb8d965909bc5b8de08fc83a85553
Kingsley <kingsley_kung@hotmail.com>
2024-04-30 16:25:38 +1000

    Added database

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -7,3 +7,21 @@ export async function loader() {
         message: "Hello, from the API!"
     })
 }
+
+export async function action({ request }) {
+    const method = request.method;
+    
+    switch (method) {
+        case "POST":
+            //Handle POST request logic here
+            //For example, adding a new item to the wishlist
+            return json({ messsage: "success", method: "POST"});
+        case "PATCH":
+            //Handle PATCH request logic here
+            //For example, updating on existing item in the wishlist
+            return json({ messsage: "success", method: "PATCH"});
+        default:
+            //Optional: handle other method or return a method not allowed response
+            return new Response("Method Not Allowed", { status: 405 });
+    }
+}
