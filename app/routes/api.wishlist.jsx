4a8621c24c79db21d9b501f071d3e6fbaae0c503
Kingsley <kingsley_kung@hotmail.com>
2024-05-04 23:21:16 +1000

    update

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,4 +1,6 @@
 import { json } from "@remix-run/node";
+import db from "../db.server";
+import { cors } from 'remix-utils/cors';
 
 export async function loader() {
 
@@ -9,19 +11,52 @@ export async function loader() {
 }
 
 export async function action({ request }) {
-    const method = request.method;
-    
+    let data = await request.formData();
+    data = Object.fromEntries(data);
+    const customerId = data.customerId;
+    const productId = data.productId;
+    const shop = data.shop;
+
+
+    if (!customerId || !productId || !shop) {
+        return json({
+            message: "Missing data. Required data: customerId, productId, shop.",
+            method: method
+        });
+    }
+
     switch (method) {
         case "POST":
-            //Handle POST request logic here
-            //For example, adding a new item to the wishlist
-            return json({ messsage: "success", method: "POST"});
+            const wishlist = await db.wishlist.create({
+                data: {
+                    customerId,
+                    productId,
+                    shop,
+                },
+            });
+            const response = json({ message: "Product added to wishlist", method: "POST", wishlist: wishlist });
+            return cors(request, response);
         case "PATCH":
-            //Handle PATCH request logic here
-            //For example, updating on existing item in the wishlist
-            return json({ messsage: "success", method: "PATCH"});
+            // Handle PATCH request logic here
+            // For example, updating an existing item in the wishlist
+            return json({ message: "Success", method: "Patch" });
+
+        case "DELETE":
+            // Handle DELETE request logic here (Not tested)
+            // For example, removing an item from the wishlist
+            await db.wishlist.deleteMany({
+                where: {
+                    customerId: customerId,
+                    shop: shop,
+                    productId: productId,
+                },
+            });
+
+            response = json({ message: "Product removed from your wishlist", method: _action, wishlisted: false });
+            return cors(request, response);
+
         default:
-            //Optional: handle other method or return a method not allowed response
+            // Optional: handle other methods or return a method not allowed response
             return new Response("Method Not Allowed", { status: 405 });
     }
 }
--- remix.config.js
+++ remix.config.js
@@ -17,4 +17,16 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
+  serverDependenciesToBundle: [
+		/^remix-utils.*/,
+		// If you installed is-ip optional dependency you will need these too
+		"is-ip",
+		"ip-regex",
+		"super-regex",
+		"clone-regexp",
+		"function-timeout",
+		"time-span",
+		"convert-hrtime",
+		"is-regexp",
+	],
 };
