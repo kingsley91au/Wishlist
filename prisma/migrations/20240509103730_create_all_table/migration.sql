4f6610a47389638f0dcca687aa7b4ac97a133b8d
Kingsley <kingsley_kung@hotmail.com>
2024-05-10 10:35:40 +1000

    Updated

--- prisma/migrations/20240509103730_create_all_table/migration.sql
+++ prisma/migrations/20240509103730_create_all_table/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Session" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
+    "shop" TEXT,
     "state" TEXT NOT NULL,
     "isOnline" BOOLEAN NOT NULL DEFAULT false,
     "scope" TEXT,
